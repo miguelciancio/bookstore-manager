1c83491dbe6d4c2b1895790f8e20b6d208a72236
miguelciancio <mciancio.figueiredo@gmail.com>
2023-02-19 19:36:26 +0000

    Created Apps Main Window

---
+++ app.py
@@ -0,0 +1,15 @@
+from PyQt5 import QtWidgets, uic
+import sys
+
+
+#  Create a base class that will load the .ui file in the constructor
+class Ui(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Ui, self).__init__()  # Call the inherited classes __init__ method
+        uic.loadUi('app_gui.ui', self)  # Load the .ui file
+        self.show()  # Show the GUI
+
+
+app = QtWidgets.QApplication(sys.argv)
+window = Ui()
+app.exec_()  # Start the application
