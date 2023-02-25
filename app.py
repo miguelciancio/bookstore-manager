d039e25da4d2fc48d568db801b7ee42816224861
miguelciancio <mciancio.figueiredo@gmail.com>
2023-02-25 18:54:53 +0000

    Created function that loads table data on screen

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from PyQt5 import QtWidgets, uic
 import sys
-from ebookstore import create_database, add_data
+from ebookstore import create_database, add_data, grab_all_data
 
 
 # Create thew third window.
@@ -18,6 +18,27 @@ class DeleteBookWindow(QtWidgets.QMainWindow):
 
         # Event Listener: capture when user clicks return button.
         self.returnMainWindow_pushButton.clicked.connect(self.returnMainWindow)
+
+        # Execute function that loads all info of books table from the bookshop database (ebookstore.db).
+        self.load_data()
+
+
+    def load_data(self):
+        datas = grab_all_data()
+
+        # Set the total number of rows according to the length of the table books
+        # from the ebookstore.db database
+        self.tableWidget.setRowCount(len(datas))
+
+        # Get each individual data of each row from the table books and
+        # display them into our screen (our table) in a user-friendly way.
+        table_row = 0
+        for row in datas:
+            self.tableWidget.setItem(table_row, 0, QtWidgets.QTableWidgetItem(str(row[0])))
+            self.tableWidget.setItem(table_row, 1, QtWidgets.QTableWidgetItem(row[1]))
+            self.tableWidget.setItem(table_row, 2, QtWidgets.QTableWidgetItem(row[2]))
+            self.tableWidget.setItem(table_row, 3, QtWidgets.QTableWidgetItem(str(row[3])))
+            table_row += 1
 
     def returnMainWindow(self):
         """Function that return to the main window when user clicks 'return' button."""
@@ -80,7 +101,6 @@ add_data(3002, "Harry Potter and the Philosopher's Stone", 'J.K. Rowling', 40)
 add_data(3003, 'The Lion, the Witch and the Wardrobe', 'C.S. Lewis', 25)
 add_data(3004, 'The Lord of the Rings', 'J.R.R. Tolkien', 37)
 add_data(3005, 'Alice in Wonderland', 'Lewis Carroll', 12)
-
 
 # ===== Main Application Execution =====
 app = QtWidgets.QApplication(sys.argv)
