f95d14648474dc15c7fb354fd45a9756594ea005
miguelciancio <mciancio.figueiredo@gmail.com>
2023-02-25 18:07:56 +0000

    Created bookshop database

--- app.py
+++ app.py
@@ -1,5 +1,29 @@
 from PyQt5 import QtWidgets, uic
 import sys
+from ebookstore import create_database, add_data
+
+
+# Create thew third window.
+class DeleteBookWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()  # Call the inherited classes __init__ method
+        self.main_window = None
+        uic.loadUi('app_gui_3.ui', self)
+
+        # Set-up size of columns
+        self.tableWidget.setColumnWidth(0, 100)
+        self.tableWidget.setColumnWidth(1, 400)
+        self.tableWidget.setColumnWidth(2, 600)
+        self.tableWidget.setColumnWidth(3, 100)
+
+        # Event Listener: capture when user clicks return button.
+        self.returnMainWindow_pushButton.clicked.connect(self.returnMainWindow)
+
+    def returnMainWindow(self):
+        """Function that return to the main window when user clicks 'return' button."""
+        self.main_window = Ui()
+        self.main_window.show()
+        self.close()
 
 
 # Create the second window.
@@ -23,12 +47,16 @@ class AddNewBookWindow(QtWidgets.QMainWindow):
 class Ui(QtWidgets.QMainWindow):
     def __init__(self):
         super(Ui, self).__init__()  # Call the inherited classes __init__ method
+        self.delete_book_window = None
         self.add_new_book_window = None
         uic.loadUi('app_gui.ui', self)  # Load the .ui file
         self.show()  # Show the GUI
 
-        # Event Listener: captures when user clicks add button.
+        # Event Listener: captures when user clicks 'add' button.
         self.addNewBook_pushButton.clicked.connect(self.AddNewBookWindow)
+
+        # Event Listener: captures when user clicks 'delete' button.
+        self.deleteBook_pushButton.clicked.connect(self.DeleteBookWindow)
 
     def AddNewBookWindow(self):
         """Function that opens the second window (add new book) when user clicks 'add' button."""
@@ -36,7 +64,25 @@ class Ui(QtWidgets.QMainWindow):
         self.add_new_book_window.show()
         self.close()
 
+    def DeleteBookWindow(self):
+        """Function that opens the third window (delete book) when user clicks 'delete' button."""
+        self.delete_book_window = DeleteBookWindow()
+        self.delete_book_window.show()
+        self.close()
 
+
+# Create a database and add initial values into it.
+create_database()
+
+# Add initial values to our database.
+add_data(3001, 'A Tale of Two Cities', 'Charles Dickens', 30)
+add_data(3002, "Harry Potter and the Philosopher's Stone", 'J.K. Rowling', 40)
+add_data(3003, 'The Lion, the Witch and the Wardrobe', 'C.S. Lewis', 25)
+add_data(3004, 'The Lord of the Rings', 'J.R.R. Tolkien', 37)
+add_data(3005, 'Alice in Wonderland', 'Lewis Carroll', 12)
+
+
+# ===== Main Application Execution =====
 app = QtWidgets.QApplication(sys.argv)
 window = Ui()
 app.exec_()  # Start the application
---
+++ ebookstore.py
@@ -0,0 +1,80 @@
+import sqlite3
+
+def create_database():
+    """Function that creates a database that for a bookshop."""
+    # Creates or opens a databse file called ebookstore.
+    db = sqlite3.connect('ebookstore.db')
+
+    # Get a cursor
+    cursor = db.cursor()
+
+    # Create a table.
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS 
+            books(
+                id INTEGER PRIMARY KEY,
+                title TEXT,
+                author TEXT,
+                quantity INTEGER
+            )
+        ''')
+
+    # Commit the changes
+    db.commit()
+
+    # Close our connection.
+    db.close()
+
+
+def add_data(id, title=None, author=None, quantity=None):
+    """Function that adds any a new book to the bookshop database."""
+    # Open bookshop database.
+    db = sqlite3.connect('ebookstore.db')
+
+    # Get a cursor
+    cursor = db.cursor()
+
+    # Insert the data if not inside it.
+    data = [
+        id,
+        title,
+        author,
+        quantity
+    ]
+
+    cursor.execute("""
+        INSERT OR IGNORE INTO 
+            books(
+                id, 
+                title, 
+                author, 
+                quantity
+            ) 
+        VALUES(?,?,?,?)
+    """, data)
+
+    # Commit changes.
+    db.commit()
+
+    # Close connection
+    db.close()
+
+
+def check_database():
+    # Open database
+    db = sqlite3.connect('ebookstore.db')
+
+    # get cursor
+    cursor = db.cursor()
+
+    # fetch all data and save into a variable
+    cursor.execute("""
+        SELECT
+            *
+        FROM 
+            books 
+    """)
+    records = cursor.fetchall()
+
+    # Print out database info
+    print(records)
