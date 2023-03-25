0945bb4b3bb2042f598402d9009bbfb4561c15b2
miguelciancio <mciancio.figueiredo@gmail.com>
2023-03-25 08:13:19 +0000

    Created FifthWindow class

--- app.py
+++ app.py
@@ -1,9 +1,114 @@
 from PyQt5 import QtWidgets, uic
 import sys
-from ebookstore import create_database, grab_last_id, add_data, grab_all_data, delete_book_data
+from ebookstore import create_database, grab_last_id, add_data, grab_all_data, delete_book_data, update_book_data
 
 
-# Create thew third window.
+# ===== 4th WINDOW - UPDATE BOOK DATA =====
+class QTableWidgetDisabledItem(QtWidgets.QItemDelegate):
+    """
+    Create a readOnly QTableWidgetItem
+    """
+
+    def __init__(self, parent):
+        QtWidgets.QItemDelegate.__init__(self, parent)
+
+    def createEditor(self, parent, option, index):
+        item = QtWidgets.QLineEdit(parent)
+        item.setReadOnly(True)
+        # item.setEnabled(False)
+        return item
+
+    def setEditorData(self, editor, index):
+        editor.blockSignals(True)
+        editor.setText(index.model().data(index))
+        editor.blockSignals(False)
+
+    def setModelData(self, editor, model, index):
+        model.setData(index, editor.text())
+
+
+class UpdateBookWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()  # Call the inherited classes __init__ method
+        self.cell = None
+        self.main_window = None
+        uic.loadUi('app_gui_4.ui', self)  # Load the GUI - Update Book Window.
+
+        # Event Listener: capture moment that user clicks 'return' button.
+        self.returnMainWindow_pushButton.clicked.connect(self.returnMainWindow)
+
+        # Display the book entries on screen
+
+        # Variable that will be used to indentify what column user wants to change.
+        self.column_name = None
+
+        # Variable that store the new value of the cell.
+        self.current_item = None
+
+        # Variable that will store the ID unique primary key number of each book.
+        self.id_number = None
+
+        # Set-up size of each column.cd
+        self.tableWidget.setColumnWidth(0, 100)
+        self.tableWidget.setColumnWidth(1, 400)
+        self.tableWidget.setColumnWidth(2, 600)
+        self.tableWidget.setColumnWidth(3, 100)
+
+        # Make the ID column to be read only; Still can click in it but nothing is gonna happens.
+        self.Size = QTableWidgetDisabledItem(self.tableWidget)
+        self.tableWidget.setItemDelegateForColumn(0, self.Size)
+
+        # Execute function that loads all info of books table from the bookshop database (ebookstore.db).
+        self.load_data()
+
+        # Event Listener: capture moment that user double-clicks a specific item of the table from 'update book' window.
+        self.tableWidget.itemDoubleClicked.connect(self.onItemDoubleClicked)
+
+        # Event Listener: capture moment that user clicks 'update' button.
+        self.update_pushButton.clicked.connect(self.update_book)
+
+    def returnMainWindow(self):
+        """Simple function that returns to the main window when user clicks 'return' button."""
+        self.main_window = Ui()
+        self.main_window.show()
+        self.close()
+
+    def load_data(self):
+        """Function that load all the data stored inside the database table's books and display them all on screen."""
+        self.datas = grab_all_data()
+
+        # Set the total number of rows according to the length of the table books
+        # from the ebookstore.db database
+        self.tableWidget.setRowCount(len(self.datas))
+
+        # Get each individual data of each row from the table books and
+        # display them into our screen (our table) in a user-friendly way.
+        table_row = 0
+        for row in self.datas:
+            self.tableWidget.setItem(table_row, 0, QtWidgets.QTableWidgetItem(str(row[0])))
+            self.tableWidget.setItem(table_row, 1, QtWidgets.QTableWidgetItem(row[1]))
+            self.tableWidget.setItem(table_row, 2, QtWidgets.QTableWidgetItem(row[2]))
+            self.tableWidget.setItem(table_row, 3, QtWidgets.QTableWidgetItem(str(row[3])))
+            table_row += 1
+
+    def update_book(self):
+        """Function that change some specific item of any book that the user wants to from bookshop database."""
+        self.current_item = self.tableWidget.currentItem().text()
+
+        self.current_col = self.tableWidget.currentColumn()
+
+        update_book_data(self.current_item, self.id_number, self.current_col)
+
+        self.load_data()
+
+    def onItemDoubleClicked(self, item):
+        """Function that get the original item that the user wants to modify."""
+        item = self.tableWidget.currentRow().text()
+
+        print(item)
+
+
+# ===== 3rd WINDOW - DELETE BOOK =====
 class DeleteBookWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()  # Call the inherited classes __init__ method
@@ -21,7 +126,7 @@ class DeleteBookWindow(QtWidgets.QMainWindow):
         self.tableWidget.setColumnWidth(2, 600)
         self.tableWidget.setColumnWidth(3, 100)
 
-        # Event Listener: capture when user clicks return button.
+        # Event Listener: capture when user clicks 'return' button.
         self.returnMainWindow_pushButton.clicked.connect(self.returnMainWindow)
 
         # Execute function that loads all info of books table from the bookshop database (ebookstore.db).
@@ -32,7 +137,6 @@ class DeleteBookWindow(QtWidgets.QMainWindow):
 
         # Event Listener: capture moment that user clicks on 'delete' button from 'delete book' window.
         self.deleteBook_pushButton.clicked.connect(self.deleteBook)
-
 
     def load_data(self):
         """Function that load all the data stored inside the database table's books and display them all on screen."""
@@ -58,7 +162,6 @@ class DeleteBookWindow(QtWidgets.QMainWindow):
         self.main_window.show()
         self.close()
 
-
     def deleteBook(self):
         """Functions that actually exclude item from database."""
         self.cell = []  # List that will store all cells values from the selected row.
@@ -80,10 +183,17 @@ class DeleteBookWindow(QtWidgets.QMainWindow):
         self.col = col
 
 
-# Create the second window.
+# ===== 2nd WINDOW - ADD NEW BOOK =====
 class AddNewBookWindow(QtWidgets.QMainWindow):
+    create_database()
+    add_data(3001, 'A Tale of Two Cities', 'Charles Dickens', 30)
+
     def __init__(self):
         super().__init__()  # Call the inherited classes __init__ method
+        self.add_quantity = None
+        self.add_author = None
+        self.add_title = None
+        self.id = None
         uic.loadUi('app_gui_2.ui', self)  # Load the .ui file
 
         self.main_window = None
@@ -117,11 +227,17 @@ class AddNewBookWindow(QtWidgets.QMainWindow):
         self.add_author = self.author.text().strip().title()
         self.add_quantity = self.quantity.text().strip().title()
 
-        # Add new data to database
-        if not self.add_title or self.add_author or self.add_quantity:
-            pass
-        else:
+        # Add new book to database only if all
+        # input fields were filled in; Otherwise
+        # do not execute anything.
+        if (self.id and
+                self.add_title and
+                self.add_author and
+                self.add_quantity
+        ):
             add_data(self.id, self.add_title, self.add_author, self.add_quantity)
+        else:
+            pass
 
         # CLear all inputs fields after successfully add the data into bookshop database.
         self.clearInput()
@@ -133,12 +249,15 @@ class AddNewBookWindow(QtWidgets.QMainWindow):
         self.quantity.setText("")
 
 
+# ===== 1st WINDOW - MAIN APP WINDOW =====
 #  Create a base class that will load the .ui file in the constructor
 class Ui(QtWidgets.QMainWindow):
     def __init__(self):
         super(Ui, self).__init__()  # Call the inherited classes __init__ method
-        self.delete_book_window = None
         self.add_new_book_window = None
+        self.delete_book_window = None
+        self.update_book_window = None
+
         uic.loadUi('app_gui.ui', self)  # Load the .ui file
         self.show()  # Show the GUI
 
@@ -147,6 +266,9 @@ class Ui(QtWidgets.QMainWindow):
 
         # Event Listener: captures when user clicks 'delete' button.
         self.deleteBook_pushButton.clicked.connect(self.DeleteBookWindow)
+
+        # Event Listener: captures when user clicks 'update' button.
+        self.updateBook_pushButton.clicked.connect(self.UpdateBookWindow)
 
     def AddNewBookWindow(self):
         """Function that opens the second window (add new book) when user clicks 'add' button."""
@@ -158,6 +280,12 @@ class Ui(QtWidgets.QMainWindow):
         """Function that opens the third window (delete book) when user clicks 'delete' button."""
         self.delete_book_window = DeleteBookWindow()
         self.delete_book_window.show()
+        self.close()
+
+    def UpdateBookWindow(self):
+        """Function that opens the fourth window (update book) when user clicks 'update' button."""
+        self.update_book_window = UpdateBookWindow()
+        self.update_book_window.show()
         self.close()
 
 
